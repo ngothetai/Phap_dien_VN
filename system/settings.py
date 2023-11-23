fd189e7d29b969b80e93e575a144796c11789e96
nguyenvanvutlv <nguyenvanvu.tlv@gmail.com>
2023-11-23 23:32:59 +0700

    :zap: update static, media root in settings

--- system/settings.py
+++ system/settings.py
@@ -130,3 +130,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/templates/images/'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'templates/images')

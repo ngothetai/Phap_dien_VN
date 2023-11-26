d9d95438cc3845eba14954dd2be1dac5bfb8adc0
nguyenvanvutlv <nguyenvanvu.tlv@gmail.com>
2023-11-26 17:39:08 +0700

    add file testcase api view tree

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
+
+
+class TreeAPITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_get_tree(self):
+        response = self.client.get(reverse('get_tree_view'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
